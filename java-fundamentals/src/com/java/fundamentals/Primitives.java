31e6c25b7ebeb65fec5185f0bcdb7ff0f2a4ef4c
anil2319 <mudhirajanil325@gmail.com>
2026-04-03 15:14:54 +0530

    feat: primitives added

---
+++ java-fundamentals/src/com/java/fundamentals/Primitives.java
@@ -0,0 +1,41 @@
+package com.java.fundamentals;
+
+public class Primitives{
+    static void main() {
+        //primitives
+        //byte
+        //stores 1 byte of memory
+        //range : -128 to +127
+        byte age;
+        byte subject1Marks, subject2Marks, subject3Marks, subject4Marks, subject5Marks;
+        age = 120;
+        System.out.println("age" + age);
+        subject1Marks = 12;
+        subject2Marks = 23;
+        subject3Marks = 45;
+        subject4Marks = 80;
+        subject5Marks = 90;
+        System.out.println("subject1Marks" + subject1Marks);
+        System.out.println("subject1Marks" + subject1Marks + ", subject2Marks" + subject2Marks + ", subject3Marks" + subject3Marks + ", subject4Marks" + subject4Marks + ", subject5Marks" + subject5Marks);
+
+        //short
+        //-32,768 to = 32,767
+        short totalMarks = 234;
+        short manufacturingYear = 2003;
+        short daysInExpiry = 340;
+        System.out.println("totalMarks =" + totalMarks + ", manufacturingYear =" + manufacturingYear + ", daysInExpiry=" + daysInExpiry);
+
+        //int
+        // -2,147,483 to 2,147,483,647
+        int salary = 50000;
+        int bankBalance = 675438;
+        System.out.println("salary" + salary);
+        System.out.println("bankBalance = " + bankBalance);
+        System.out.println(" salary =" + salary + ", bankBalance =," + bankBalance);
+
+        //long
+        //8 bytes
+        //
+
+    }
+}
