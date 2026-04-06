4a70742744f117ac8431845c45878f5e9c8c4a85
anil2319 <mudhirajanil325@gmail.com>
2026-04-06 13:00:34 +0530

    initial-commit

---
+++ java-datatypes/src/ecommerce.java
@@ -0,0 +1,31 @@
+public class ecommerce {
+    public static void main(String[] args) {
+
+        // Product Variables
+        System.out.println("----------prooduct variables-------");
+
+        int productId = 105;
+        String productName = "Watch";
+        float productPrice =  10999.99f;
+        char productGrade = 'A';
+        Boolean instock = true;
+
+        // Customer Variebles
+        System.out.println("--------customer variables------");
+
+        int customerId = 321;
+        String customerName = "Anil";
+        long phoneNumber = 7075471092L;
+        char gender = 'M';
+        boolean isPeimemember = true;
+
+        // Order Variables
+        System.out.println("---------order varibles------");
+
+        int orderId = 8787;
+        double orderAmount = 10999.99;
+        int quantity = 1;
+        boolean isDelivered = false;
+
+    }
+}
