3b6eed846ed2d408e4c009270f0556e42ff27068
anil2319 <mudhirajanil325@gmail.com>
2026-05-14 16:23:14 +0530

    feet: datatypes

---
+++ java-datatypes/src/com/java/datatypes/StudentData.java
@@ -0,0 +1,15 @@
+package com.java.datatypes;
+
+import java.util.Scanner;
+
+public class StudentData {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Roll no :");
+        byte rollNo = sc.nextByte();
+        System.out.println(" marks");
+        short marks = sc.nextShort();
+        System.out.println("roll no" + rollNo);
+        System.out.println("marks " + marks);
+    }
+}
